70b403a30fa225193c324157a73abc0e0a588b58
Luis Suarez <suarezluis@gmail.com>
2019-01-24 20:23:43 -0600

    added folder for labs

---
+++ Labs/TT17_L0_Suarez.cpp
@@ -0,0 +1,12 @@
+//This is how to write a comment
+
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "What is the current year? ";
+	int currentYear = 0;
+	cin >> currentYear;
+	cout << "Next year is: " << ++currentYear << endl;
+	cout << "Good-bye!\n";
+	return 0;
+}
